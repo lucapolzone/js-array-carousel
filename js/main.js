19682b8f0091d5ec46d63414e1a323ca2b37de9c
lucapolzone <lucapolzone@gmail.com>
2024-01-18 15:37:42 +0100

    feat: add images array & create "for loop" for carousel

--- js/main.js
+++ js/main.js
@@ -4,3 +4,32 @@ const wrapperSlides = document.querySelector(".wrapper_slides");
 const arrowUp = document.getElementById("arrow-up");
 const arrowDown = document.getElementById("arrow-down");
 
+// Creo array delle immagini
+
+const slides = [
+  "01.webp",
+  "02.webp",
+  "03.webp",
+  "04.webp",
+  "05.webp"
+];
+
+//Seleziono l'immagine che mostro
+let slideShowed = 0;
+
+//genero le slides
+let slidesHtml;
+
+for (let i = 0; i < slides.length; i++) {
+  //recupero ogni elemento
+  const singleSlide = slides[i];
+
+  let classActive;
+  if (i== slideShowed) {
+    classActive = "active";
+  }
+
+  //inserisco in html
+  slidesHtml += `<img src="./img/${singleSlide}" class="slide ${classActive}">`
+}
+
