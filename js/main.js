88cb10dff8ce7ef27e1bbb3d3087a6ac6b7e7a45
lucapolzone <lucapolzone@gmail.com>
2024-01-18 15:36:24 +0100

    feat: get html elements

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Elementi HTML
+
+const wrapperSlides = document.querySelector(".wrapper_slides");
+const arrowUp = document.getElementById("arrow-up");
+const arrowDown = document.getElementById("arrow-down");
+
